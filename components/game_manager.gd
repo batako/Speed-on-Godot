c9adad40928399e35897384d1fd5605696909a23
batako <guri725ren213@gmail.com>
2024-02-11 23:22:16 +0900

    山札からカードを引くことができるように

--- components/game_manager.gd
+++ components/game_manager.gd
@@ -4,12 +4,47 @@ extends Node
 @export var CardRootNode: Node
 @export var CardScene: PackedScene
 
-var instance_cards: Array[Area2D] = []
+@export var PlayerDeckSlot: Node
+@export var PlayerCardSlot1: Node
+@export var PlayerCardSlot2: Node
+@export var PlayerCardSlot3: Node
+@export var PlayerCardSlot4: Node
+
+@export var EnemyDeckSlot: Node
+@export var EnemyCardSlot1: Node
+@export var EnemyCardSlot2: Node
+@export var EnemyCardSlot3: Node
+@export var EnemyCardSlot4: Node
+
+var player_card_deck: Array
+var enemy_card_deck: Array
+
 
 func _ready() -> void:
 	setup_cards()
 
+
+func _draw_player_card() -> void:
+	draw_card("player")
+
+
+func _draw_enemy_card() -> void:
+	draw_card("enemy")
+
+
+func _selected_card(suit: String, value: int) -> void:
+	print("_selected_card")
+	print(suit + " : " + str(value))
+
+
 func setup_cards() -> void:
+	setup_deck()
+
+	add_card(PlayerDeckSlot, "player", "deck", 0)
+	add_card(EnemyDeckSlot, "enemy", "deck", 1)
+	
+
+func create_shuffled_deck() -> Array:
 	var suits = ["club", "diamond", "heart", "spade"]
 	var cards = []
 	
@@ -19,21 +54,94 @@ func setup_cards() -> void:
 	
 	cards.shuffle()
 	
-	var y = 0
-	for card in cards:
-		add_card(card.suit, card.value, y)
-		y += 20
+	return cards
 
-func add_card(suit: String, value: int, y: int) -> void:
+
+func setup_deck() -> void:
+	var deck: Array = create_shuffled_deck()
+	var deck_half_size: int = deck.size() / 2
+	var player_cards: Array = deck.slice(0, deck_half_size - 1, true)
+	var enemy_cards: Array = deck.slice(deck_half_size, deck.size() - 1, true)
+
+	player_card_deck = player_cards
+	enemy_card_deck = enemy_cards
+
+
+func add_card(root_node: Node, owner_type: String, suit: String, value: int) -> void:
+	if owner_type != "player" and owner_type != "enemy" and owner_type != "field":
+		push_error("不正な owner_type です。:  " + owner_type)
+		return
+	
 	var card_instance: Area2D = CardScene.instantiate()
 	card_instance.name = "%s_%d" % [suit.capitalize(), value]
-	card_instance.position.x = 44 + y
-	card_instance.position.y = 62
-	card_instance.setup(suit, value)
-	CardRootNode.add_child(card_instance)
-	instance_cards.append(card_instance)
-	card_instance.connect("selected_card", _selected_card.bind())
+	card_instance.setup(owner_type, suit, value)
+	root_node.add_child(card_instance)
 
-func _selected_card(suit: String, value: int) -> void:
-	print("_selected_card")
-	print(suit + " : " + str(value))
+	match suit:
+		"deck":
+			if owner_type == "player":
+				card_instance.connect("draw_player_card", _draw_player_card.bind())
+			elif owner_type == "enemy":
+				card_instance.connect("draw_enemy_card", _draw_enemy_card.bind())
+			else:
+				push_error("不正な owner_type です。:  " + owner_type)
+		_:
+			card_instance.connect("selected_card", _selected_card.bind())
+
+
+func draw_card(owner_type: String) -> void:
+	if owner_type != "player" and owner_type != "enemy":
+		push_error("不正な owner_type です。:  " + owner_type)
+		return
+	
+	var slot = get_empty_field_hand_slot(owner_type)
+	if slot:
+		var card = draw_card_from_deck(owner_type)
+		if card.size() != 0:
+			add_card(slot, owner_type, card.suit, card.value)
+	else:
+		push_error("空きスロットがないため引くことができません。")
+
+
+func get_empty_field_hand_slot(owner_type: String) -> Node:
+	var card_slots = get_field_hand_slots(owner_type)
+	return find_first_empty_child_node(card_slots)
+
+
+func get_field_hand_slots(owner_type: String) -> Array:
+	match owner_type:
+		"player":
+			return [PlayerCardSlot1, PlayerCardSlot2, PlayerCardSlot3, PlayerCardSlot4]
+		"enemy":
+			return [EnemyCardSlot1, EnemyCardSlot2, EnemyCardSlot3, EnemyCardSlot4]
+		_:
+			push_error("不正な owner_type です。:  " + owner_type)
+			return []
+
+
+func find_first_empty_child_node(nodes: Array) -> Node:
+	for node in nodes:
+		if node.get_child_count() == 0:
+			return node
+	return null
+
+
+func draw_card_from_deck(owner_type: String) -> Dictionary:
+	var card_deck = get_card_deck(owner_type)
+	
+	if card_deck.size() > 0:
+		return card_deck.pop_front()
+	else:
+		push_error(owner_type + " の山札はありません。")
+		return {}
+
+
+func get_card_deck(owner_type: String) -> Array:
+	match owner_type:
+		"player":
+			return player_card_deck
+		"enemy":
+			return enemy_card_deck
+		_:
+			push_error("不正な owner_type です。:  " + owner_type)
+			return []
--- scenes/card/card.gd
+++ scenes/card/card.gd
@@ -3,16 +3,23 @@ extends Area2D
 @onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
 @onready var collision_shape: CollisionShape2D = $CollisionShape2D
 
+enum OwnerType { PLAYER, ENEMY, FIELD }
+
+var owner_type: OwnerType
 var is_face_up: bool = false
 var suit: String
 var value: int
 
 signal selected_card(suit: String, value: int)
+signal draw_player_card
+signal draw_enemy_card
+
 
 func _ready() -> void:
 	var script_path = get_script().resource_path
+	assert(owner_type >= 0, str(script_path) + " の owner_type が設定されていません。")
 	assert(suit, str(script_path) + " の suit が設定されていません。")
-	assert(value, str(script_path) + " の value が設定されていません。")
+	assert(value >= 0, str(script_path) + " の value が設定されていません。")
 	
 	connect("input_event", _on_input_event.bind())
 	connect("mouse_entered", _on_mouse_entered.bind())
@@ -20,28 +27,61 @@ func _ready() -> void:
 	
 	turnFaceUp()
 
+
 func _on_mouse_entered() -> void:
 	Global.set_pointing()
+
 
 func _on_mouse_exited() -> void:
 	Global.set_arrow()
 
+
 func _on_input_event(_viewport: Node, event: InputEvent, _shape_idx: int) -> void:
 	if event is InputEventMouseButton:
 		if event.is_pressed():
-			emit_signal("selected_card", suit, value)
+			if suit == "deck":
+				emit_signal("draw_" + owner_type_to_string() + "_card")
+			else:
+				emit_signal("selected_card", suit, value)
 
-func setup(_suit: String, _value: int) -> void:
+
+func owner_type_to_string() -> String:
+	match owner_type:
+		OwnerType.PLAYER:
+			return "player"
+		OwnerType.ENEMY:
+			return "enemy"
+		OwnerType.FIELD:
+			return "field"
+		_:
+			push_error("Invalid owner type: " + str(owner_type))
+			return "Invalid"
+
+
+func setup(_owner_type: String, _suit: String, _value: int) -> void:
+	match _owner_type:
+		"player":
+			owner_type = OwnerType.PLAYER
+		"enemy":
+			owner_type = OwnerType.ENEMY
+		"field":
+			owner_type = OwnerType.FIELD
+		_:
+			push_error("Invalid owner type: " + _owner_type)
+	
 	suit = _suit
 	value = _value
+
 
 func turnFaceUp() -> void:
 	is_face_up = true
 	set_animation_and_frame(suit, value)
 
+
 func turnFaceDown() -> void:
 	is_face_up = false
 	set_animation_and_frame("back", 0)
+
 
 func set_animation_and_frame(animation_name: String, frame_number: int) -> void:
 	animated_sprite.animation = animation_name
