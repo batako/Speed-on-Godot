8f71f9b6ac669073dad06d827ec5bc3596b3fead
batako <guri725ren213@gmail.com>
2024-02-14 23:47:24 +0900

    使用していない引数の先頭に_を付加

--- scenes/count_down/count_down.gd
+++ scenes/count_down/count_down.gd
@@ -7,7 +7,7 @@ func _ready() -> void:
 	$AnimationPlayer.connect("animation_finished", _on_animation_player_animation_finished.bind())
 
 
-func _on_animation_player_animation_finished(anim_name: StringName) -> void:
+func _on_animation_player_animation_finished(_anim_name: StringName) -> void:
 	visible = false
 	emit_signal("animation_finished")
 
